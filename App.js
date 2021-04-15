b7d04daaffd2a3c6046939ae0e65570b66869274
nknaveen007 <connaveen007@gmail.com>
2021-04-15 03:38:00 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Jitsi from './components/Jitsi';
+
+
+
+const Section = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App= () =>  {
+  const [JoinMetting, setJoinMetting] = useState(false)
+
+  return (
+    <View style={styles.container}>
+      {JoinMetting?<Jitsi/>: <TouchableOpacity onPress={()=>setJoinMetting(true)}  style={{borderWidth:1,paddingVertical:10,paddingHorizontal:30}}>
+        <Text>Join Meeting </Text>
+      </TouchableOpacity>}
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
